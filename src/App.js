61d566d12d0c4a7f2fee109b4c5383ec5e253abd
Your Name <you@example.com>
2021-05-09 02:58:04 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [likeColor, setLikeColor] = useState('');
+  const [users, setUser] = useState([]);
+  const [singleUser, setSingleUser] = useState({});
+  const [randomUser, setRandomUser] = useState({});
+  useState(() => {
+    // Users
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json ())
+    .then(data => {
+      // console.log(data);
+      setUser(data)
+    })
+
+    // Single User
+    fetch('https://jsonplaceholder.typicode.com/users/1')
+    .then(response => response.json())
+    .then(data => {
+      // console.log(data);
+      setSingleUser(data)
+    })
+
+    //Random Users
+    fetch('https://randomuser.me/api/')
+    .then(response => response.json ())
+    .then(data => {
+      // console.log(data);
+      setRandomUser(data.results[0])
+    })
+  },[])
+
+ 
+  const handleLike = () => {
+    const color = likeColor ? '' : 'primary';
+    setLikeColor(color);
+    // setLikeColor(likeColor ? '' : 'primary');
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <AccessAlarmIcon></AccessAlarmIcon>
+        <ThumbUpAltIcon style={{color: 'blue'}}></ThumbUpAltIcon>
+        <ThumbUpAltIcon color="primary"></ThumbUpAltIcon>
+        <ThumbUpAltIcon onClick={handleLike} color={likeColor}></ThumbUpAltIcon>
+        <ThumbUpAltIcon onClick={() => setLikeColor(likeColor ? '' : 'primary')} color={likeColor}></ThumbUpAltIcon>
+        <h1>Name: {singleUser.name}</h1>
+        <h2>Random User Gender: {randomUser.gender}</h2>
+
+        {/* ************************************************************************************************
+        ********************************important */}
+        <h2>Random Name 1: {randomUser.name && randomUser.name.first}</h2>
+        <h2>Random Name 2: {randomUser.name?.first}</h2>
+
+        
+        {
+          users.map(user => <li>{user.name}</li>)
+        }
+        
     </div>
   );
 }
